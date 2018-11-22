aed91ac23ae547bd1548cf508e9fbdeaece86f2b
OCGTenno <44306386+OCGTenno@users.noreply.github.com>
2018-11-22 09:02:05 -0500

    Added Rotations to all the enemy wheels, updated the Enemy and Utility Methods scripts and added a new script "Autoscaler"

--- 18-making-a-tower-defense-game/starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/Enemy.cs
+++ 18-making-a-tower-defense-game/starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/Enemy.cs
@@ -84,7 +84,7 @@ public class Enemy : MonoBehaviour
 
         //Move to next waypoint
         transform.position = Vector3.MoveTowards(transform.position, targetPosition,moveSpeed * Time.deltaTime);
-        transform.LookAt(targetPosition);
+        transform.localRotation = UtilityMethods.SmoothlyLook(transform, targetPosition);
 
         //Set target to next waypoint when the Enemy is close.
         if (Vector3.Distance(transform.position, targetPosition) < .1f)
